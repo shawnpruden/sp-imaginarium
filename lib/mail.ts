6b7c5d9394f292411885fde925ccfcdfaa58fbe4
Shawn Pruden <echt.shawn@gmail.com>
2024-07-22 16:29:27 +0900

    changed mail configs

--- lib/mail.ts
+++ lib/mail.ts
@@ -8,7 +8,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const passwordResetLink = `${domain}/reset-password?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'no-reply@sp-imaginarium.vercel.app',
     to: email,
     subject: 'Reset your password',
     html: `<p>Click <a href="${passwordResetLink}">here</a> to reset your password.</p>`,
@@ -19,7 +19,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmationLink = `${domain}/verify-email?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'no-reply@sp-imaginarium.vercel.app',
     to: email,
     subject: 'Confirm your email',
     html: `<p>Click <a href="${confirmationLink}">here</a> to confirm your email.</p>`,
