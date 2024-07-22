0627556f4147edcbbcf05b356363def49f7cb3aa
Shawn Pruden <echt.shawn@gmail.com>
2024-07-22 11:21:38 +0900

    added env variable for mail

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,10 +2,10 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const DOMAIN = 'http://localhost:3000';
+const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const passwordResetLink = `${DOMAIN}/reset-password?token=${token}`;
+  const passwordResetLink = `${domain}/reset-password?token=${token}`;
 
   await resend.emails.send({
     from: 'onboarding@resend.dev',
@@ -16,7 +16,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 };
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmationLink = `${DOMAIN}/verify-email?token=${token}`;
+  const confirmationLink = `${domain}/verify-email?token=${token}`;
 
   await resend.emails.send({
     from: 'onboarding@resend.dev',
